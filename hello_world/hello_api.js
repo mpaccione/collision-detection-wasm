7d3d1bc8c52a88d0e601e4ea47ce9d1f52d7984b
Michael Paccione <mpaccione1991@gmail.com>
2019-11-30 19:53:16 -0800

    Hello World WASM WIP

--- hello_world/hello_api.js
+++ hello_world/hello_api.js
@@ -6,16 +6,11 @@ const app = express();
 
 app.use(cors());
 
-app.options("wasm", function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.end();
-});
-
 app.get("/wasm", function(req, res, next) {
 	console.log('WASM Endpoint Requested');
-	res.send(wasm);
+	console.log(wasm);
+	res.writeHead(200, {'Content-Type': 'application/wasm'});
+	res.end(wasm);
 });
 
 app.listen(5000, () => console.log('Example app listening on port 5000!'))
