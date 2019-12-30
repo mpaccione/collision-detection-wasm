afc1360f6c981bcb381ff6d02026210a95bcccb4
Michael Paccione <mpaccione1991@gmail.com>
2019-12-30 12:42:18 -0800

    Started C++ Version

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # collision-detection-wasm
+
+### Javascript Version
+
+Open js_version.html in browser. Uses p5.js library for quick rendering.
+
+### C++ Version
+
+In terminal you can build using the included simple2d library. Due to bug in simple2d (https://github.com/simple2d/simple2d/issues/118). You will need to build via this command.
+```
+g++ c-code-test.cpp `simple2d --libs` -o c-code-test
+```
+
+To run the output
+```
+./c-code-test
+```
+
+### WASM Version
+
+Coming soon!
+
+
--- c-code-test.cpp
+++ c-code-test.cpp
@@ -1,5 +1,9 @@
+// Due to dependency bug compile with command
+// g++ c-code-test.cpp `simple2d --libs` -o c-code-test
+
 #include <iostream>
 #include <queue>
+#include <simple2d.h>
 
 #define HEIGHT 600
 #define WIDTH  800
@@ -26,32 +30,74 @@ struct Ball {
 
 };
 
+// GLOBALS
+queue<Ball> objQueue;
 
-void moveBall(Ball obj){
-	if (obj.posX > WIDTH){ obj.speedX = (rand() % 10 + 1) * -1; }; 
-    if (obj.posY > HEIGHT){ obj.speedY = (rand() % 10 + 1) * -1; };
-    if (obj.posX < 0){ obj.speedX = rand() % 10 + 1; };
-    if (obj.posY < 0){ obj.speedY = rand() % 10 + 1; };
+void moveBall(Ball b){
+	if (b.posX > WIDTH){ b.speedX = (rand() % 10 + 1) * -1; }; 
+    if (b.posY > HEIGHT){ b.speedY = (rand() % 10 + 1) * -1; };
+    if (b.posX < 0){ b.speedX = rand() % 10 + 1; };
+    if (b.posY < 0){ b.speedY = rand() % 10 + 1; };
 
-    obj.posX += obj.speedX;
-  	obj.posY += obj.speedY;
+    b.posX += b.speedX;
+  	b.posY += b.speedY;
 }
 
-void updatePos(Ball obj){
-
+void drawBall(Ball b){
+	S2D_DrawCircle( b.posX, b.posY, b.size/2, 360, 255, 255, 255, 1 );
 }
 
-void collisionCheck(){
+void collisionCheck(Ball b){
+	// Javascript Code to be converted to C++
 
+	// for (var i = 0; i < a; i++) {
+
+ //  		//Improved Col Detection - Takes Larger Radius of the Two Balls
+ //  		var colRadius = ballArray[i].size > this.size ? ballArray[i].size*0.5 : this.size*0.5;
+
+ //  		if (colRadius > 1) {
+		// 		if (this.posX <= posArray[i][0] + colRadius && this.posX >= posArray[i][0] - colRadius
+		// 			&& this.posY <= posArray[i][1] + colRadius && this.posY >= posArray[i][1] - colRadius)
+		// 		{
+		// 			//Reverse Direction 
+		// 			this.speedX *= -0.66;
+		// 			this.speedY *= -0.66;
+		// 			ballArray[i].speedX *= -0.66;
+		// 			ballArray[i].speedY *= -0.66;
+
+		// 			//Make 2/3 Original Size
+		// 			this.size *= 0.66;
+		// 			ballArray[i].size *= 0.66;
+
+		// 			//Add New Ball on Col
+		// 			var newBall = new ball(ballArray[i].posX + ballArray[i].size, ballArray[i].posY + ballArray[i].size, 8.3, 8.3, ballArray[i].size);
+		// 			ballArray.push(newBall);
+		// 		}
+		// 	} 
+ //  	}
 }
 
+// LOGGING
 std::ostream &operator<<(std::ostream &os, const Ball &b) { 
 	return os << "pX: " << b.posX << "\npY: " << b.posY << "\nsX: " << b.speedX << "\nsY: " << b.speedY << "\n" << endl;
 }
 
-int main(){
-	queue<Ball> objQueue;
+void update(){
+	// Update Application State
+}
 
+void render(){
+	// TEST
+	S2D_DrawCircle( 100, 100, 50, 360, 255, 255, 255, 1 );
+
+	// Draw Stuff
+	// while (!objQueue.empty()){
+	// 	drawBall(objQueue.front());
+	// 	objQueue.pop();
+	// }
+}
+
+int main(){
 	Ball obj1(10, 10, 5, 5, 150);
 	Ball obj2(1490, 740, 5, 5, 150);
 	Ball obj3(10, 740, 5, 5, 150); 
@@ -62,11 +108,17 @@ int main(){
 	objQueue.push( obj3 );
 	objQueue.push( obj4 );
 
-	// TEST
-	while (!objQueue.empty()){
-		cout << objQueue.front();
-		objQueue.pop();
-	}
+	// TEST - LOGGING
+	// while (!objQueue.empty()){
+	// 	cout << objQueue.front();
+	// 	objQueue.pop();
+	// }
 
+	// 2D Drawing 
+	S2D_Window *window = S2D_CreateWindow(
+		"2D Collision Detection", 800, 600, NULL, render, 0
+	);
+
+	S2D_Show(window);
 	return 0;
 }
