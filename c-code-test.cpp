5fa7c84de8c24780c091cf1da9113bd14f4ba10d
Michael Paccione <mpaccione1991@gmail.com>
2019-12-30 19:22:26 -0800

    Collision Detection C++

--- c-code-test.cpp
+++ c-code-test.cpp
@@ -5,8 +5,8 @@
 #include <vector>
 #include <simple2d.h>
 
-#define HEIGHT 1920
-#define WIDTH  1680
+#define HEIGHT  800
+#define WIDTH  1024
 
 using namespace std;
 
@@ -34,44 +34,46 @@ struct Ball {
 vector<Ball> bVector;
 
 void moveBall(Ball &b){
-	if (b.posX > WIDTH){ b.speedX = (rand() % 10 + 1) * -1; }; 
-    if (b.posY > HEIGHT){ b.speedY = (rand() % 10 + 1) * -1; };
-    if (b.posX < 0){ b.speedX = rand() % 10 + 1; };
-    if (b.posY < 0){ b.speedY = rand() % 10 + 1; };
+	int const radius = b.size/2;
+
+	if (b.posX > (WIDTH + radius)){ b.speedX = (rand() % 10 + 1) * -1; }; 
+    if (b.posY > (HEIGHT + radius)){ b.speedY = (rand() % 10 + 1) * -1; };
+    if ((b.posX - radius) < 0){ b.speedX = rand() % 10 + 1; };
+    if ((b.posY - radius) < 0){ b.speedY = rand() % 10 + 1; };
 
     b.posX += b.speedX;
-  	b.posY += b.speedY;
+    b.posY += b.speedY;
 }
 
-//void collisionCheck(Ball &b){
-	// Javascript Code to be converted to C++
+void collisionCheck(Ball &b1, int n){
+	for (int i = 0; i < bVector.size(); i++) {
+		if (i != n){
+  			// Improved Col Detection - Takes Larger Radius of the Two Balls
+  			Ball& b2 = bVector[i];
+  			int const colRadius = b2.size > b1.size ? b2.size*0.5 : b1.size*0.5;
+  			// Only visible balls
+  			if (colRadius > 1) {
+				if (b1.posX <= b2.posX + colRadius && b1.posX >= b2.posX - colRadius
+					&& b1.posY <= b2.posY + colRadius && b1.posY >= b2.posY - colRadius)
+				{
+					// Reverse Direction 
+					b1.speedX *= -0.66;
+					b1.speedY *= -0.66;
+					b2.speedX *= -0.66;
+					b2.speedY *= -0.66;
 
-	// for (var i = 0; i < a; i++) {
+					// Make 2/3 Original Size
+					b1.size *= 0.66;
+					b2.size *= 0.66;
 
- //  		//Improved Col Detection - Takes Larger Radius of the Two Balls
- //  		var colRadius = ballArray[i].size > this.size ? ballArray[i].size*0.5 : this.size*0.5;
-
- //  		if (colRadius > 1) {
-		// 		if (this.posX <= posArray[i][0] + colRadius && this.posX >= posArray[i][0] - colRadius
-		// 			&& this.posY <= posArray[i][1] + colRadius && this.posY >= posArray[i][1] - colRadius)
-		// 		{
-		// 			//Reverse Direction 
-		// 			this.speedX *= -0.66;
-		// 			this.speedY *= -0.66;
-		// 			ballArray[i].speedX *= -0.66;
-		// 			ballArray[i].speedY *= -0.66;
-
-		// 			//Make 2/3 Original Size
-		// 			this.size *= 0.66;
-		// 			ballArray[i].size *= 0.66;
-
-		// 			//Add New Ball on Col
-		// 			var newBall = new ball(ballArray[i].posX + ballArray[i].size, ballArray[i].posY + ballArray[i].size, 8.3, 8.3, ballArray[i].size);
-		// 			ballArray.push(newBall);
-		// 		}
-		// 	} 
- //  	}
-//}
+					// Add New Ball on Col
+					Ball b3(b2.posX + b2.size, b2.posY + b2.size, 8.3, 8.3, b2.size);
+					bVector.push_back(b3);
+				}
+			}
+		} 
+  	}
+}
 
 // DEBUGGING - LOGGING
 std::ostream &operator<<(std::ostream &os, const Ball &b) { 
@@ -83,7 +85,7 @@ void update(){
 	for (int i = 0; i < bVector.size(); ++i){
 		Ball& b = bVector.at(i);
 		moveBall(b);
-		// cout << "posX " << b.posX << " posY " << b.posY << endl;
+		collisionCheck(b, i);
 	}
 }
 
@@ -96,10 +98,10 @@ void render(){
 }
 
 int main(){
-	Ball b1(10, 10, 5, 5, 150);
-	Ball b2(1490, 740, 5, 5, 150);
-	Ball b3(10, 740, 5, 5, 150); 
-	Ball b4(1490, 10, 5, 5, 150); 
+	Ball b1(10, 10, 5, 5, 100);
+	Ball b2(1490, 740, 5, 5, 100);
+	Ball b3(10, 740, 5, 5, 100); 
+	Ball b4(1490, 10, 5, 5, 100); 
 
 	bVector.push_back(b1);
 	bVector.push_back(b2);
